032d8edc64e72e65a4e63cb058ef5cad81c14b85
ArasuRever <arasu7747@gmail.com>
2025-11-11 18:37:29 +0530

    2.25% intrest added to LoanForm.js

--- src/components/LoanForm.js
+++ src/components/LoanForm.js
@@ -131,7 +131,7 @@ function LoanForm({ customerId, onLoanAdded }) {
                   <option value="1.0">1.0%</option>
                   <option value="1.5">1.5%</option>
                   <option value="2.0">2.0%</option>
-                  <option value="2.5">2.25%</option>
+                  <option value="2.25">2.25%</option>
                   <option value="2.5">2.5%</option>
                   <option value="3.0">3.0%</option>
                   <option value="3.5">3.5%</option>
